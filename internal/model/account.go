60bfb8053f59a04b5a31c73d072a7d5e8f4e3f7d
liyasong <94583573+liyasongAha@users.noreply.github.com>
2021-11-22 15:45:13 +0800

    fix(account):删除了 BeforeCreate,BeforeUpdate (#32)

--- internal/model/account.go
+++ internal/model/account.go
@@ -42,20 +42,7 @@ func (a *Account) AfterFind(tx *gorm.DB) (err error) {
 	return nil
 }
 
-func (a *Account) BeforeCreate(tx *gorm.DB) (err error) {
-	return a.aesBeforeOperation(tx)
-}
-
 func (a *Account) BeforeSave(tx *gorm.DB) (err error) {
-	return a.aesBeforeOperation(tx)
-
-}
-
-func (a *Account) BeforeUpdate(tx *gorm.DB) (err error) {
-	return a.aesBeforeOperation(tx)
-}
-
-func (a *Account) aesBeforeOperation(tx *gorm.DB) (err error) {
 	if a == nil {
 		return nil
 	}
