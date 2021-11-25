3cf9a10da4c3f5bab5c4978d71de7353bcd4f8cf
liyasong <94583573+liyasongAha@users.noreply.github.com>
2021-11-25 15:21:08 +0800

    feature(middleware):validator (#48)
    * feature(middleware):Use `for range` and compare char with numberMap,upperLetterMap,lowerLetterMap,specialCharMap instead of regexp.Add gin validator test.

---
+++ cmd/api/middleware/validation/validator_test.go
@@ -0,0 +1,169 @@
+package validation
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/go-playground/validator/v10"
+)
+
+const (
+	sevenNumbers     = "1234567"
+	thirtyOneNumbers = "1234567890123456789012345678901"
+	tenNumbers       = "1234567890"
+	tenUpperLetters  = "aaaaaaaaaa"
+	tenLowerLetters  = "AAAAAAAAAA"
+	tenSpecialChar   = "??????????"
+)
+
+func testValidator() (*validator.Validate, error) {
+	v := validator.New()
+	RegisterCustomValidators()
+	err := RegisterValidators(v)
+	if err != nil {
+		return nil, err
+	}
+	return v, nil
+}
+
+func Test_validateAllCharacter(t *testing.T) {
+	type args struct {
+		Password string
+	}
+	errMsg := fmt.Sprintf("[Password] %s", CharTypeGT3TransErr)
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "验证长度小于8",
+			args: args{
+				Password: sevenNumbers,
+			},
+			want: "Password长度必须至少为8个字符",
+		},
+		{
+			name: "验证长度大于30",
+			args: args{
+				Password: thirtyOneNumbers,
+			},
+			want: "Password长度不能超过30个字符",
+		},
+		{
+			name: "验证纯数字",
+			args: args{
+				Password: tenNumbers,
+			},
+			want: errMsg,
+		},
+		{
+			name: "验证纯大写字母",
+			args: args{
+				Password: tenUpperLetters,
+			},
+			want: errMsg,
+		},
+		{
+			name: "验证纯小写字母",
+			args: args{
+				Password: tenLowerLetters,
+			},
+			want: errMsg,
+		},
+		{
+			name: "验证纯特殊字符",
+			args: args{
+				Password: tenSpecialChar,
+			},
+			want: errMsg,
+		},
+		{
+			name: "验证2种字符类型:数字+大写字母",
+			args: args{
+				Password: tenNumbers + tenUpperLetters,
+			},
+			want: errMsg,
+		},
+		{
+			name: "验证2种字符类型:数字+小写字母",
+			args: args{
+				Password: tenNumbers + tenLowerLetters,
+			},
+			want: errMsg,
+		},
+		{
+			name: "验证2种字符类型:数字+特殊字符",
+			args: args{
+				Password: tenNumbers + tenSpecialChar,
+			},
+			want: errMsg,
+		},
+		{
+			name: "验证2种字符类型:大写字母+小写字母",
+			args: args{
+				Password: tenUpperLetters + tenLowerLetters,
+			},
+			want: errMsg,
+		},
+		{
+			name: "验证2种字符类型:大写字母+特殊字符",
+			args: args{
+				Password: tenUpperLetters + tenSpecialChar,
+			},
+			want: errMsg,
+		},
+		{
+			name: "验证2种字符类型:小写字母+特殊字符",
+			args: args{
+				Password: tenLowerLetters + tenSpecialChar,
+			},
+			want: errMsg,
+		},
+		{
+			name: "验证3种字符类型:数字+大写字母+小写字母",
+			args: args{
+				Password: tenNumbers + tenUpperLetters + tenLowerLetters,
+			},
+			want: "",
+		},
+		{
+			name: "验证3种字符类型:数字+大写字母+特殊字符",
+			args: args{
+				Password: tenNumbers + tenUpperLetters + tenSpecialChar,
+			},
+			want: "",
+		},
+		{
+			name: "验证3种字符类型:数字+小写字母+特殊字符",
+			args: args{
+				Password: tenNumbers + tenLowerLetters + tenSpecialChar,
+			},
+			want: "",
+		},
+		{
+			name: "验证3种字符类型:大写字母+小写字母+特殊字符",
+			args: args{
+				Password: tenUpperLetters + tenLowerLetters + tenSpecialChar,
+			},
+			want: "",
+		},
+	}
+	type ValidateCase struct {
+		Password string `validate:"min=8,max=30,charTypeGT3"`
+	}
+	v, err := testValidator()
+	if err != nil {
+		t.Fatalf("init validator failed.err:[%s]", err.Error())
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := ValidateCase{Password: tt.args.Password}
+			err := v.Struct(c)
+			if got := Translate2Chinese(err); got != tt.want {
+				t.Errorf("allchar failed. got:[%s] want:[%s]", got, tt.want)
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -51,7 +51,7 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/go-playground/validator/v10 v10.9.0
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang/protobuf v1.5.2 // indirect
