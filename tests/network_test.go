a99e8bb429e05a1fdbcf87feafb02babeeea1011
yangliqun <yangliqun@galaxy-future.com>
2021-12-27 21:08:06 +0800

    fix go vet test

--- tests/network_test.go
+++ tests/network_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/galaxy-future/BridgX/internal/model"
 	"github.com/galaxy-future/BridgX/internal/service"
-	"github.com/galaxy-future/BridgX/internal/types"
 )
 
 func TestCreateVPC(t *testing.T) {
@@ -238,16 +237,7 @@ func TestGetVPC(t *testing.T) {
 					VpcName:    "测试一键创建",
 					PageNumber: 0,
 					PageSize:   20,
-					Account: &types.OrgKeys{
-						OrgId: 0,
-						//Info: []types.Info{
-						//	{
-						//		AK:       "",
-						//		SK:       "",
-						//		Provider: "",
-						//	},
-						//},
-					},
+					AccountKey: "",
 				},
 			},
 			wantVpcs: nil,
