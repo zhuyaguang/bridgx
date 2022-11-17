56d418feaa6ef1e2c791ae015c48578820ca370d
朱亚光 <349608856@qq.com>
2022-11-17 21:33:52 +0800

    [Task][TencentCloud] Support TCR Operation #197 (#220)
    * [Task][TencentCloud] Support TCR Operation #197
    
    * fix error handle

--- pkg/cloud/tencent/cr.go
+++ pkg/cloud/tencent/cr.go
@@ -1,31 +1,170 @@
 package tencent
 
-import "github.com/galaxy-future/BridgX/pkg/cloud"
+import (
+	"fmt"
 
-func (p *TencentCloud) PersonalImageList(region, repoNamespace, repoName string, pageNum, pageSize int) ([]cloud.DockerArtifact, int, error) {
-	return []cloud.DockerArtifact{}, 0, nil
+	"github.com/galaxy-future/BridgX/pkg/cloud"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/errors"
+	tcr "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/tcr/v20190924"
+)
+
+func (p *TencentCloud) PersonalImageList(instanceId, repoNamespace, repoName string, pageNum, pageSize int) ([]cloud.DockerArtifact, int, error) {
+	request := tcr.NewDescribeImagesRequest()
+	request.RegistryId = &instanceId
+	request.NamespaceName = &repoNamespace
+	request.RepositoryName = &repoName
+
+	var DockerArtifactList []cloud.DockerArtifact
+
+	// 返回的resp是一个DescribeImagesResponse的实例，与请求对象对应
+	response, err := p.tcrClient.DescribeImages(request)
+	if _, ok := err.(*errors.TencentCloudSDKError); ok {
+		fmt.Printf("An API error has returned: %s", err)
+		return DockerArtifactList, 0, err
+	}
+	if err != nil {
+		return DockerArtifactList, 0, err
+	}
+	for _, d := range response.Response.ImageInfoList {
+		var docker cloud.DockerArtifact
+		docker.Name = *d.Digest
+		DockerArtifactList = append(DockerArtifactList, docker)
+	}
+	return DockerArtifactList, int(*response.Response.TotalCount), nil
 }
 
 func (p *TencentCloud) EnterpriseImageList(region, instanceId, repoId, namespace, repoName string, pageNumber, pageSize int) ([]cloud.DockerArtifact, int, error) {
-	return []cloud.DockerArtifact{}, 0, nil
+	request := tcr.NewDescribeImagesRequest()
+	request.RegistryId = &instanceId
+	request.NamespaceName = &namespace
+	request.RepositoryName = &repoName
+
+	var DockerArtifactList []cloud.DockerArtifact
+	// 返回的resp是一个DescribeImagesResponse的实例，与请求对象对应
+	response, err := p.tcrClient.DescribeImages(request)
+	if _, ok := err.(*errors.TencentCloudSDKError); ok {
+		fmt.Printf("An API error has returned: %s", err)
+		return DockerArtifactList, 0, err
+	}
+	if err != nil {
+		return DockerArtifactList, 0, err
+	}
+
+	for _, d := range response.Response.ImageInfoList {
+		var docker cloud.DockerArtifact
+		docker.Name = *d.Digest
+		DockerArtifactList = append(DockerArtifactList, docker)
+	}
+	return DockerArtifactList, int(*response.Response.TotalCount), nil
 }
 
 func (p *TencentCloud) ContainerInstanceList(region string, pageNumber, pageSize int) ([]cloud.RegistryInstance, int, error) {
-	return []cloud.RegistryInstance{}, 0, nil
+
+	request := tcr.NewDescribeInstancesRequest()
+	var RegistryInstanceList []cloud.RegistryInstance
+
+	response, err := p.tcrClient.DescribeInstances(request)
+	if _, ok := err.(*errors.TencentCloudSDKError); ok {
+		fmt.Printf("An API error has returned: %s", err)
+		return RegistryInstanceList, 0, err
+	}
+	if err != nil {
+		return RegistryInstanceList, 0, err
+	}
+
+	for _, r := range response.Response.Registries {
+		var registryInstance cloud.RegistryInstance
+		registryInstance.InstanceId = *r.RegistryId
+		registryInstance.InstanceName = *r.RegionName
+		RegistryInstanceList = append(RegistryInstanceList, registryInstance)
+	}
+
+	return RegistryInstanceList, int(*response.Response.TotalCount), nil
 }
 
 func (p *TencentCloud) EnterpriseNamespaceList(region, instanceId string, pageNumber, pageSize int) ([]cloud.Namespace, int, error) {
-	return []cloud.Namespace{}, 0, nil
+	request := tcr.NewDescribeNamespacesRequest()
+	var NamespaceList []cloud.Namespace
+	request.RegistryId = common.StringPtr(instanceId)
+
+	response, err := p.tcrClient.DescribeNamespaces(request)
+	if _, ok := err.(*errors.TencentCloudSDKError); ok {
+		fmt.Printf("An API error has returned: %s", err)
+		return NamespaceList, 0, err
+	}
+	if err != nil {
+		return NamespaceList, 0, err
+	}
+	for _, n := range response.Response.NamespaceList {
+		namespace := cloud.Namespace{Name: *n.Name}
+		NamespaceList = append(NamespaceList, namespace)
+	}
+	return NamespaceList, int(*response.Response.TotalCount), nil
 }
 
 func (p *TencentCloud) PersonalNamespaceList(region string) ([]cloud.Namespace, error) {
-	return []cloud.Namespace{}, nil
+	request := tcr.NewDescribeNamespacePersonalRequest()
+	request.Namespace = common.StringPtr("")
+	request.Limit = common.Int64Ptr(10)
+	request.Offset = common.Int64Ptr(0)
+	var NamespaceList []cloud.Namespace
+
+	response, err := p.tcrClient.DescribeNamespacePersonal(request)
+	if _, ok := err.(*errors.TencentCloudSDKError); ok {
+		fmt.Printf("An API error has returned: %s", err)
+		return NamespaceList, err
+	}
+	if err != nil {
+		return NamespaceList, err
+	}
+	for _, n := range response.Response.Data.NamespaceInfo {
+		namespace := cloud.Namespace{Name: *n.Namespace}
+		NamespaceList = append(NamespaceList, namespace)
+	}
+	return NamespaceList, nil
 }
 
 func (p *TencentCloud) EnterpriseRepositoryList(region, instanceId, namespace string, pageNumber, pageSize int) ([]cloud.Repository, int, error) {
-	return []cloud.Repository{}, 0, nil
+	request := tcr.NewDescribeRepositoriesRequest()
+	var RepositoryList []cloud.Repository
+	request.RegistryId = common.StringPtr(instanceId)
+
+	response, err := p.tcrClient.DescribeRepositories(request)
+	if _, ok := err.(*errors.TencentCloudSDKError); ok {
+		fmt.Printf("An API error has returned: %s", err)
+		return RepositoryList, 0, err
+	}
+	if err != nil {
+		return RepositoryList, 0, err
+	}
+	for _, r := range response.Response.RepositoryList {
+		repository := cloud.Repository{
+			Name: *r.Name,
+			ID:   "",
+		}
+		RepositoryList = append(RepositoryList, repository)
+	}
+	return RepositoryList, int(*response.Response.TotalCount), nil
 }
 
 func (p *TencentCloud) PersonalRepositoryList(region, namespace string, pageNumber, pageSize int) ([]cloud.Repository, int, error) {
-	return []cloud.Repository{}, 0, nil
+	request := tcr.NewDescribeRepositoryOwnerPersonalRequest()
+	var RepositoryList []cloud.Repository
+	response, err := p.tcrClient.DescribeRepositoryOwnerPersonal(request)
+	if _, ok := err.(*errors.TencentCloudSDKError); ok {
+		fmt.Printf("An API error has returned: %s", err)
+		return RepositoryList, 0, err
+	}
+	if err != nil {
+		return RepositoryList, 0, err
+	}
+	for _, r := range response.Response.Data.RepoInfo {
+		repository := cloud.Repository{
+			Name: *r.RepoName,
+			ID:   "",
+		}
+		RepositoryList = append(RepositoryList, repository)
+	}
+	return RepositoryList, int(*response.Response.Data.TotalCount), nil
 }
--- pkg/cloud/tencent/cr_test.go
+++ pkg/cloud/tencent/cr_test.go
@@ -1,1 +1,93 @@
 package tencent
+
+import "testing"
+
+func TestContainerInstanceList(t *testing.T) {
+	client, err := New(_ak, _sk, "ap-beijing")
+	if err != nil {
+		t.Log(err.Error())
+		return
+	}
+	res, count, err := client.ContainerInstanceList("", 0, 0)
+	t.Log(count)
+	for _, b := range res {
+		t.Log(b.InstanceId, b.InstanceName)
+	}
+}
+
+func TestEnterpriseNamespaceList(t *testing.T) {
+	client, err := New(_ak, _sk, "ap-beijing")
+	if err != nil {
+		t.Log(err.Error())
+		return
+	}
+	res, count, err := client.EnterpriseNamespaceList("", "test", 0, 0)
+	t.Log(count)
+	for _, b := range res {
+		t.Log(b.Name)
+	}
+}
+
+func TestPersonalNamespaceList(t *testing.T) {
+	client, err := New(_ak, _sk, "ap-beijing")
+	if err != nil {
+		t.Log(err.Error())
+		return
+	}
+	res, err := client.PersonalNamespaceList("")
+	for _, b := range res {
+		t.Log(b.Name)
+	}
+}
+
+func TestEnterpriseRepositoryList(t *testing.T) {
+	client, err := New(_ak, _sk, "ap-beijing")
+	if err != nil {
+		t.Log(err.Error())
+		return
+	}
+	res, count, err := client.EnterpriseRepositoryList("", "test", "", 0, 0)
+	t.Log(count)
+	for _, b := range res {
+		t.Log(b.Name, b.ID)
+	}
+}
+
+func TestPersonalRepositoryList(t *testing.T) {
+	client, err := New(_ak, _sk, "ap-beijing")
+	if err != nil {
+		t.Log(err.Error())
+		return
+	}
+	res, count, err := client.PersonalRepositoryList("", "", 0, 0)
+	t.Log(count)
+	for _, b := range res {
+		t.Log(b.Name, b.ID)
+	}
+}
+
+func TestEnterpriseImageList(t *testing.T) {
+	client, err := New(_ak, _sk, "ap-beijing")
+	if err != nil {
+		t.Log(err.Error())
+		return
+	}
+	res, count, err := client.EnterpriseImageList("", "", "test", "test", "test", 0, 0)
+	t.Log(count)
+	for _, b := range res {
+		t.Log(b.Name)
+	}
+}
+
+func TestPersonalImageList(t *testing.T) {
+	client, err := New(_ak, _sk, "ap-beijing")
+	if err != nil {
+		t.Log(err.Error())
+		return
+	}
+	res, count, err := client.PersonalImageList("", "", "test", 0, 0)
+	t.Log(count)
+	for _, b := range res {
+		t.Log(b.Name)
+	}
+}
