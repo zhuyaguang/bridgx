034a993fe318bd00ce3d0735365b110ce588682e
binginx <sdaulibin@126.com>
2022-11-22 16:23:06 +0800

    Feat task [移动云] Support KeyPair Operation #208 (#224)
    * feat: task 20221102 [移动云]Support KeyPair Operation #208
    Co-authored-by: binginx baiyutang

--- pkg/cloud/ecloud/keypair_test.go
+++ pkg/cloud/ecloud/keypair_test.go
@@ -1,1 +1,54 @@
 package ecloud
+
+import (
+	"github.com/galaxy-future/BridgX/internal/logs"
+	"github.com/galaxy-future/BridgX/pkg/cloud"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var (
+	_AK       = ""
+	_SK       = ""
+	_regionId = "cn-north-1"
+)
+
+func init() {
+	logs.Init()
+}
+
+func TestEcloud_DescribeKeyPairs(t *testing.T) {
+	p, err := New(_AK, _SK, _regionId)
+	if err != nil {
+		t.Errorf("err:%v", err)
+		return
+	}
+	describeKeyPairs, err := p.DescribeKeyPairs(cloud.DescribeKeyPairsRequest{
+		PageSize:   10,
+		PageNumber: 1})
+
+	if err != nil {
+		t.Errorf("err:%v", err)
+		return
+	}
+	t.Log(describeKeyPairs.KeyPairs)
+	assert.Equal(t, describeKeyPairs.TotalCount, len(describeKeyPairs.KeyPairs))
+}
+
+func TestEcloud_CreateKeyPair(t *testing.T) {
+	p, err := New(_AK, _SK, _regionId)
+	if err != nil {
+		t.Errorf("err:%v", err)
+		return
+	}
+	createKeyPair, err := p.CreateKeyPair(cloud.CreateKeyPairRequest{
+		KeyPairName: "test002",
+	})
+
+	if err != nil {
+		t.Errorf("err:%v", err)
+		return
+	}
+	t.Log(createKeyPair)
+	assert.Equal(t, createKeyPair.KeyPairName, "test002")
+}
