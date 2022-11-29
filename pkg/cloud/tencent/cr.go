959bba62f89c21d8aabfb7347f8a4eb15c2d7326
zhuyaguang <zhuyaguang1368@163.com>
2022-11-29 12:37:37 +0800

    通过获取repo来获取namespaces

--- pkg/cloud/tencent/cr.go
+++ pkg/cloud/tencent/cr.go
@@ -6,6 +6,7 @@ import (
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/errors"
 	tcr "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/tcr/v20190924"
+	"strings"
 )
 
 //此处与阿里云对齐，各名称定义如下示例
@@ -19,7 +20,8 @@ func (p *TencentCloud) PersonalImageList(region, repoNamespace, repoName string,
 	var DockerArtifactList []cloud.DockerArtifact
 
 	request := tcr.NewDescribeImagePersonalRequest()
-	request.RepoName = &repoName
+	ComRepoName := repoNamespace + "/" + repoName
+	request.RepoName = &ComRepoName
 	request.Offset, request.Limit = SizeNumConvert(pageNumber, pageSize)
 	// 返回的resp是一个DescribeImagePersonalResponse的实例，与请求对象对应
 	response, err := p.tcrClient.DescribeImagePersonal(request)
@@ -112,23 +114,41 @@ func (p *TencentCloud) EnterpriseNamespaceList(region, instanceId string, pageNu
 }
 
 func (p *TencentCloud) PersonalNamespaceList(region string) ([]cloud.Namespace, error) {
-	request := tcr.NewDescribeNamespacePersonalRequest()
-	request.Namespace = common.StringPtr("")
-	request.Offset, request.Limit = SizeNumConvert(1, 10)
 	var NamespaceList []cloud.Namespace
-
-	response, err := p.tcrClient.DescribeNamespacePersonal(request)
+	requestROP := tcr.NewDescribeRepositoryOwnerPersonalRequest()
+	response, err := p.tcrClient.DescribeRepositoryOwnerPersonal(requestROP)
 	if _, ok := err.(*errors.TencentCloudSDKError); ok {
 		logs.Logger.Errorf("An API error has returned: %s", err)
-		return NamespaceList, err
+		return nil, err
 	}
 	if err != nil {
-		return NamespaceList, err
+		return nil, err
 	}
-	for _, n := range response.Response.Data.NamespaceInfo {
-		namespace := cloud.Namespace{Name: *n.Namespace}
-		NamespaceList = append(NamespaceList, namespace)
+	for _, r := range response.Response.Data.RepoInfo {
+		namespace := ""
+		name := *r.RepoName
+		resultArr := strings.Split(name, "/")
+		if len(resultArr) != 0 {
+			namespace = resultArr[0]
+		}
+		NamespaceList = append(NamespaceList, cloud.Namespace{Name: namespace})
 	}
+	//request := tcr.NewDescribeNamespacePersonalRequest()
+	//request.Namespace = &namespace
+	//request.Offset, request.Limit = SizeNumConvert(0, 10)
+	//response, err := p.tcrClient.DescribeNamespacePersonal(request)
+	//if _, ok := err.(*errors.TencentCloudSDKError); ok {
+	//	logs.Logger.Errorf("An API error has returned: %s", err)
+	//	return NamespaceList, err
+	//}
+	//if err != nil {
+	//	return NamespaceList, err
+	//}
+	//for _, n := range response.Response.Data.NamespaceInfo {
+	//	namespace := cloud.Namespace{Name: *n.Namespace}
+	//	NamespaceList = append(NamespaceList, namespace)
+	//}
+
 	return NamespaceList, nil
 }
 
