95ecea9068f0ebbfd923bdd4b012f38c83b96fae
zhuyaguang <zhuyaguang1368@163.com>
2022-12-29 10:21:56 +0800

    提交工单让移动修复了ims SDK ,增加了镜像接口

--- pkg/cloud/ecloud/api.go
+++ pkg/cloud/ecloud/api.go
@@ -1,6 +1,8 @@
 package ecloud
 
 import (
+	"errors"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -8,14 +10,14 @@ import (
 	"gitlab.ecloud.com/ecloud/ecloudsdkcore/config"
 	"gitlab.ecloud.com/ecloud/ecloudsdkecs"
 	"gitlab.ecloud.com/ecloud/ecloudsdkeip"
-	//"gitlab.ecloud.com/ecloud/ecloudsdkims"
-	//"gitlab.ecloud.com/ecloud/ecloudsdkims/model"
+	"gitlab.ecloud.com/ecloud/ecloudsdkims"
+	"gitlab.ecloud.com/ecloud/ecloudsdkims/model"
 )
 
 type ECloud struct {
-	eipClient *ecloudsdkeip.Client
-	ecsClient *ecloudsdkecs.Client
-	//imsClient  *ecloudsdkims.Client
+	eipClient  *ecloudsdkeip.Client
+	ecsClient  *ecloudsdkecs.Client
+	imsClient  *ecloudsdkims.Client
 	eosSession *session.Session
 }
 
@@ -31,11 +33,11 @@ func New(ak, sk, regionId string) (*ECloud, error) {
 		PoolId:    regionId,
 	})
 
-	//imsClient := ecloudsdkims.NewClient(&config.Config{
-	//	AccessKey: ak,
-	//	SecretKey: sk,
-	//	PoolId:    regionId,
-	//})
+	imsClient := ecloudsdkims.NewClient(&config.Config{
+		AccessKey: ak,
+		SecretKey: sk,
+		PoolId:    regionId,
+	})
 
 	endPoint := getOssEndpoint(regionId)
 	disableSSL := false
@@ -52,9 +54,9 @@ func New(ak, sk, regionId string) (*ECloud, error) {
 	}
 
 	return &ECloud{
-		eipClient: eipClient,
-		ecsClient: ecsClient,
-		//imsClient:  imsClient,
+		eipClient:  eipClient,
+		ecsClient:  ecsClient,
+		imsClient:  imsClient,
 		eosSession: eosSession,
 	}, nil
 }
@@ -63,28 +65,28 @@ func (p *ECloud) ProviderType() string {
 	return cloud.ECloud
 }
 
-//func (p *ECloud) DescribeImages(req cloud.DescribeImagesRequest) (cloud.DescribeImagesResponse, error) {
-//	request := &model.IMSgetImageRespRequest{}
-//	iMSgetImageRespPath := &model.IMSgetImageRespPath{}
-//	iMSgetImageRespPath.ImageId = req.ImageID
-//	request.IMSgetImageRespPath = iMSgetImageRespPath
-//	response, err := p.imsClient.IMSgetImageResp(request)
-//	if err == nil {
-//		fmt.Printf("%+v\n", response)
-//		if response.State == model.IMSgetImageRespResponseStateEnumOk {
-//			var images cloud.Image
-//			images.ImageName = response.Body.Name
-//			images.ImageId = response.Body.ImageId
-//			images.OsName = response.Body.OsName
-//			images.Size = int(*response.Body.Size)
-//			images.OsType = string(response.Body.OsType)
-//			images.Platform = string(response.Body.PublicImageType)
-//			return cloud.DescribeImagesResponse{Images: []cloud.Image{images}}, nil
-//		} else {
-//			err := errors.New(response.ErrorMessage)
-//			return cloud.DescribeImagesResponse{}, err
-//		}
-//	}
-//	return cloud.DescribeImagesResponse{}, err
-//
-//}
+func (p *ECloud) DescribeImages(req cloud.DescribeImagesRequest) (cloud.DescribeImagesResponse, error) {
+	request := &model.IMSgetImageRespRequest{}
+	iMSgetImageRespPath := &model.IMSgetImageRespPath{}
+	iMSgetImageRespPath.ImageId = req.ImageID
+	request.IMSgetImageRespPath = iMSgetImageRespPath
+	response, err := p.imsClient.IMSgetImageResp(request)
+	if err == nil {
+		fmt.Printf("%+v\n", response)
+		if response.State == model.IMSgetImageRespResponseStateEnumOk {
+			var images cloud.Image
+			images.ImageName = response.Body.Name
+			images.ImageId = response.Body.ImageId
+			images.OsName = response.Body.OsName
+			images.Size = int(*response.Body.Size)
+			images.OsType = string(response.Body.OsType)
+			images.Platform = string(response.Body.PublicImageType)
+			return cloud.DescribeImagesResponse{Images: []cloud.Image{images}}, nil
+		} else {
+			err := errors.New(response.ErrorMessage)
+			return cloud.DescribeImagesResponse{}, err
+		}
+	}
+	return cloud.DescribeImagesResponse{}, err
+
+}
--- pkg/cloud/ecloud/api_test.go
+++ pkg/cloud/ecloud/api_test.go
@@ -1,12 +1,17 @@
 package ecloud
 
-//func TestECloud_DescribeImages(t *testing.T) {
-//	client, err := New(_AK, _SK, "ap-beijing")
-//	if err != nil {
-//		t.Log(err.Error())
-//		return
-//	}
-//
-//	res, err := client.DescribeImages(cloud.DescribeImagesRequest{ImageID: "test"})
-//	t.Log(res.Images)
-//}
+import (
+	"github.com/galaxy-future/BridgX/pkg/cloud"
+	"testing"
+)
+
+func TestECloud_DescribeImages(t *testing.T) {
+	client, err := New(_AK, _SK, "ap-beijing")
+	if err != nil {
+		t.Log(err.Error())
+		return
+	}
+
+	res, err := client.DescribeImages(cloud.DescribeImagesRequest{ImageID: "test"})
+	t.Log(res.Images)
+}
